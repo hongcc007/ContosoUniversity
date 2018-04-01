c708f689586b84c99588e6c8165cc94c42de2478
hongcc007 <chuhongfei@gmail.com>
2018-04-01 22:48:34 +1000

    Initial commit

---
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.Models;
+using ContosoUniversity.DAL;
+using PagedList;
+
+namespace ContosoUniversity.Controllers
+{
+    public class StudentController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET: /Student/
+        public ActionResult Index(string sortOrder, string searchString)
+        {
+            ViewBag.LastNameSortParm = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewBag.FirstnameSortParm = sortOrder == "FirstName" ? "firstname_desc" : "FirstName";
+            var students = from s in db.Students select s;
+            if(!string.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(i => i.LastName.ToUpper().Contains(searchString.ToUpper())
+                    || i.FirstMidName.ToUpper().Contains(searchString.ToUpper()));
+            }
+            switch(sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(i => i.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(i => i.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(i => i.EnrollmentDate);
+                    break;
+                case "FirstName":
+                    students = students.OrderBy(i => i.FirstMidName);
+                    break;
+                case "firstname_desc":
+                    students = students.OrderByDescending(i => i.FirstMidName);
+                    break;
+                default:
+                    students = students.OrderBy(i => i.LastName);
+                    break;
+            }
+            return View(students.ToList());
+        }
+
+        // GET: /Student/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: /Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Student/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="LastName,FirstMidName,EnrollmentDate")] Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(DataException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("","Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(student);
+        }
+
+        // GET: /Student/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: /Student/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(student).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(DataException)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(student);
+        }
+
+        // GET: /Student/Delete/5
+        public ActionResult Delete(int? id, bool? saveChangesError=false)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if(saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: /Student/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                //Student student = db.Students.Find(id);
+                //db.Students.Remove(student);
+                //db.SaveChanges();
+                Student studentToDelete = new Student() { ID = id };
+                db.Entry(studentToDelete).State = EntityState.Deleted;
+                db.SaveChanges();
+            }
+            catch(DataException)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
